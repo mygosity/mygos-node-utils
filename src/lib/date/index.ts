0a80dc4dfaed7208698661ee959697cc80884edf
Michael.Lee <mygosity@gmail.com>
2019-09-02 20:06:56 +1000

    fixed types for date

--- src/lib/date/index.ts
+++ src/lib/date/index.ts
@@ -1,7 +1,7 @@
 import moment from 'moment-timezone';
 import utils from '../common';
 
-export const getDateFormat = (date: Date, formatStr: string = 'YYYY-MM-DD'): moment => {
+export const getDateFormat = (date: Date | string, formatStr: string = 'YYYY-MM-DD'): moment => {
   return moment(date).format(formatStr);
 };
 
@@ -15,7 +15,10 @@ export const timeDifference = (from: number, to: Date, type: string = 'days'): m
   return fromTime.diff(toTime, type);
 };
 
-export const getAusTimestamp = (date: number, formatStr: string = 'YYYY-MM-DD HH:mm:ss'): string => {
+export const getAusTimestamp = (
+  date: number,
+  formatStr: string = 'YYYY-MM-DD HH:mm:ss',
+): string => {
   return date
     ? moment(date)
         .tz('Australia/Sydney')
@@ -25,7 +28,9 @@ export const getAusTimestamp = (date: number, formatStr: string = 'YYYY-MM-DD HH
         .format(formatStr);
 };
 
-export const wrapWithAusTimeStamp = (obj: any): { timestamp: number; aussieTime: string; [key: string]: any } => {
+export const wrapWithAusTimeStamp = (
+  obj: any,
+): { timestamp: number; aussieTime: string; [key: string]: any } => {
   if (!utils.isObject(obj)) {
     if (Array.isArray(obj)) {
       obj = { ...obj };
