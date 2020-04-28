c2479e1c671280b852774a696c5c8d274e284f11
mygosity <mygosity@gmail.com>
2020-04-28 22:45:50 +1000

    added useful pathing function to safely navigate json data with expressive code

--- src/lib/common/objectarray/index.ts
+++ src/lib/common/objectarray/index.ts
@@ -4,6 +4,40 @@ import { Dictionary } from '../../typedefinitions';
 
 export const emptyFunctionCall = (): void => {};
 
+/**
+ * Safely return a target value by specifying an object to parse and a strPath in the form of the key props
+ * example usage for accessing 'data.seasons[0].episodes[2].id' safely
+ *   const data = path(await api.get(endpoint), ['data', 'seasons', 0, 'episodes', 2, 'id'], { defaultValue: '' });
+ *   if there's an error the defaultValue is returned or it will just be undefined
+ *   can also specify debug: true as an option in order to get a console log of where it failed
+ * @param {object} target
+ * @param {string} strPath an array of values that you will access by key
+ * @param {object} options can specify defaultValue or debug to see a console log of where it failed and why
+ */
+export function path(
+	target: { [key: string]: any },
+	strPath: string[],
+	options: { defaultValue?: any; debug?: boolean } = {},
+) {
+	if (target == null) return options.defaultValue;
+	let answer = target;
+	for (let i = 0; i < strPath.length; ++i) {
+		if (answer[strPath[i]] == null) {
+			if (options.debug) {
+				console.log(
+					'path=> key: ' +
+						strPath[i] +
+						' not found! The object before it failed to path is printed below: ',
+				);
+				console.log(answer);
+			}
+			return options.defaultValue;
+		}
+		answer = answer[strPath[i]];
+	}
+	return answer;
+}
+
 export async function promiseAllObject(
 	object: Dictionary<Promise<any>>,
 	options: {
@@ -14,9 +48,12 @@ export async function promiseAllObject(
 	for (let key in object) {
 		try {
 			object[key] =
-				options.transformFunction == null ? await object[key] : await options.transformFunction(object[key]);
+				options.transformFunction == null
+					? await object[key]
+					: await options.transformFunction(object[key]);
 		} catch (error) {
-			object[key] = options.errorHandler == null ? null : await options.errorHandler(key, error);
+			object[key] =
+				options.errorHandler == null ? null : await options.errorHandler(key, error);
 		}
 	}
 	return object;
@@ -24,7 +61,10 @@ export async function promiseAllObject(
 
 export function mergeArrayElements(arr1: any[], arr2: any[]): any[] {
 	if (arr1.length !== arr2.length) {
-		logger.report({ logSignature: 'Utils=>' }, 'mergeArrayElements:: arrays must be same length');
+		logger.report(
+			{ logSignature: 'Utils=>' },
+			'mergeArrayElements:: arrays must be same length',
+		);
 		return;
 	}
 	const result = [];
@@ -118,7 +158,11 @@ export function swapArrayElement(list: any[], fromIndex: number, toIndex: number
 	list[toIndex] = target;
 }
 
-export function swapElementToIndex(list: any[], matchCondition: Function, targetIndex: number): any[] {
+export function swapElementToIndex(
+	list: any[],
+	matchCondition: Function,
+	targetIndex: number,
+): any[] {
 	const newList = [...list];
 	let foundIndex = -1;
 	for (let i = 0; i < list.length; ++i) {
@@ -153,7 +197,11 @@ export function recursiveToString(target: any): any {
 	return answer;
 }
 
-export function deepCloneObject(target: any, dupeChecker: any[] = [], skipCircularRef: boolean): any {
+export function deepCloneObject(
+	target: any,
+	dupeChecker: any[] = [],
+	skipCircularRef: boolean,
+): any {
 	let clone = Array.isArray(target) ? [] : {};
 	if (!dupeChecker.includes(target)) {
 		dupeChecker.push(target);
